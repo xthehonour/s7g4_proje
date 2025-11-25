2f1af2d236a3f13e68ace248a95ebc7f5851574f
xthehonour <demirtasonur747@gmail.com>
2025-11-26 01:31:33 +0300

    firsComponents

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,6 +1,14 @@
+import { use, useState } from "react";
 export default function Login(){
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
 
   return (
-    <div></div>
+    <div>
+      <form>
+        <label htmlFor="email">Email: </label>
+        <input id="email" name="email"></input>
+      </form>
+    </div>
   )
 }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
+import Login from "../components/Login";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+     <Login />
     </div>
   );
 }
